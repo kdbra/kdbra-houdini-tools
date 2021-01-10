01adccb2955b19a0e62b9d6a9dd1b1547c35edc1
mrmcdobrotny <soda@crack.cr>
2021-01-11 00:42:31 +0300

    lets bring some $KDBRA_HOUDINI_ASSETS env variable into "~/.profile" that points to the right directory

--- update_houdini_tools.py
+++ update_houdini_tools.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python2.7
 import os
 
-hfs = os.environ["HFS"]
-with  os.popen("hconfig -ap") as f:
-	houdini_path = [l for l in f.readlines() if "HOUDINI_PATH :=" in l][0].split(":")
-houdini_path = [p for p in houdini_path if os.path.exists(p) and hfs not in p]
-
-print "{}".format(houdini_path)
+kdbra = os.environ["KDBRA_HOUDINI_ASSETS"]
+print kdbra
