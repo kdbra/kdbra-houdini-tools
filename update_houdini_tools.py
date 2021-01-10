1a101d73476c657f9c0fec2dc0f68cc2d88c4d86
mrmcdobrotny <soda@crack.cr>
2021-01-11 00:06:01 +0300

    testing houdini asset directories parser

--- update_houdini_tools.py
+++ update_houdini_tools.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python2.7
 import os
 
-path = os.environ["PATH"]
-hou_path = [ p for p in path.split(":") if "houdini" in p][0]
-home = os.environ["HOME"]
-print hou_path
-print home
+hfs = os.environ["HFS"]
+with  os.popen("hconfig -ap") as f:
+	houdini_path = [l for l in f.readlines() if "HOUDINI_PATH :=" in l][0].split(":")
+houdini_path = [p for p in houdini_path if os.path.exists(p) and hfs not in p]
+
+print "{}".format(houdini_path)
