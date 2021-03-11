f9859b507a5c62a5f51595fb57898a5a5608ad74
mrmcdobrotny <mr.prudov2012@gmail.com>
2021-03-11 20:05:58 +0300

    new python module containing batch funcs

---
+++ python2.7libs/batchTools.py
@@ -0,0 +1,17 @@
+import glob
+from os import path, listdir
+
+
+
+def fixCrowdCache(input_dir, search_path,new_path):
+    list_geos = glob.glob(path.join(input_dir, "*.geo"))
+    for item in list_geos:
+        tmp = ""
+        with open(item, "rw") as file:
+            for line in file:
+                tmp += line.replace(search_path,new_path)
+            
+        file = open(item, "w")
+        file.write(tmp)
+        file.close()
+        print "{} fixed".format(item)
