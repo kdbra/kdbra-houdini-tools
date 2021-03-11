ac34f75b43bbd232fa467228a0f4ae4c6e12ab39
mrmcdobrotny <mr.prudov2012@gmail.com>
2021-03-11 20:03:23 +0300

    code reviewed

--- python2.7libs/batch_gconvert.py
+++ python2.7libs/batch_gconvert.py
@@ -10,14 +10,13 @@ directory = args.input_dir
 informat = args.input_format
 outformat = args.output_format
 listdir = listdir(directory)
-cnt = 0
+
 for item in listdir:
     item_path = path.join(directory, item)
     if path.isfile(item_path):
-    	name = path.splitext(item)[0]
-    	fmt = path.splitext(item)[1]
-    	if fmt == informat:
-	    	new_path = path.join(directory,name+outformat)
-	    	subprocess.Popen(["gconvert", item_path, new_path], stdout=subprocess.PIPE)
-	    	cnt += 1
-print "{} files has been converted to .bgeo.sc".format(cnt)
+        name = path.splitext(item)[0]
+        fmt = path.splitext(item)[1]
+        if fmt == informat:
+            new_path = path.join(directory, name + outformat)
+            subprocess.run(["gconvert", item_path, new_path])
+            print "{} converted to .bgeo.sc".format(item_path)
