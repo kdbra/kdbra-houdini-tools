5c9f0005e02c28f2cba2c77a1f53f968d019705c
mrmcdobrotny <mr.prudov2012@gmail.com>
2021-03-11 20:07:57 +0300

    code prettyfied

--- python2.7libs/batchTools.py
+++ python2.7libs/batchTools.py
@@ -1,16 +1,13 @@
 import glob
 from os import path, listdir
 
-
-
 def fixCrowdCache(input_dir, search_path,new_path):
     list_geos = glob.glob(path.join(input_dir, "*.geo"))
     for item in list_geos:
         tmp = ""
-        with open(item, "rw") as file:
+        with open(item, "r") as file:
             for line in file:
                 tmp += line.replace(search_path,new_path)
-            
         file = open(item, "w")
         file.write(tmp)
         file.close()
