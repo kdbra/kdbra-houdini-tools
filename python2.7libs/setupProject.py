4f2128d70bc77e5e2aebf5dff3b953eabc062d37
sick <ilya.lindberg@gmail.com>
2021-03-22 21:52:34 +0300

    One more python rewrite :-)

--- python2.7libs/setupProject.py
+++ python2.7libs/setupProject.py
@@ -6,34 +6,25 @@ parser.add_argument('-s', "--src", type=str, help='src directory', default="./sr
 parser.add_argument('-n', '--scene_name', type=str, help='scene name', default="")
 args = parser.parse_args()
 
-def mkdir(path):
-    if not os.path.isdir(path):
-        os.mkdir(path)
-
-pwd = os.environ["PWD"]
-shots = os.path.join(pwd, "shots")
-mkdir(shots)
-
+pwd = os.getcwd()
 
 srcdir = os.path.abspath(args.src)
+
+# get shot list 
 shotslist = os.listdir(srcdir)
-scene = os.path.join(shots, args.scene_name)
-mkdir(scene)
 
 for shotname in shotslist:
-    shotdir = os.path.join(scene, shotname)
-    if os.path.isdir(os.path.join(srcdir, shotname)):
-        print "{} directory created".format(shotdir)
-        if not os.path.isdir(shotdir):
-            os.mkdir(shotdir)
-            hip = os.path.join(shotdir, "hip")
-            trk = os.path.join(shotdir, "trk")
-            nk = os.path.join(shotdir, "nk")
-            dailies = os.path.join(shotdir, "dailies")
-            proxy = os.path.join(shotdir, "proxy")
-            mkdir(shotdir)
-            mkdir(hip)
-            mkdir(trk)
-            mkdir(nk)
-            mkdir(dailies)
-            mkdir(proxy)
+    paths = []
+    # Note - you can make nested paths here    
+    dirs = ['dailies',
+             'hip',
+             'trk',
+             'trk/proxy',
+             'one/two/three',
+             'nk']
+    for i in dirs:
+        paths.append(os.path.join(pwd, 'shots', args.scene_name, shotname, i))
+
+    for item in paths:
+        if not os.path.exists(item):
+            os.makedirs(item)
