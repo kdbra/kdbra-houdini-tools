b422de0aa192779192ae09a55f33a2f093dc5b5a
mrmcdobrotny <mr.prudov2012@gmail.com>
2021-03-29 15:13:15 +0300

    render time check func added

--- python2.7libs/batchTools.py
+++ python2.7libs/batchTools.py
@@ -1,5 +1,5 @@
-import glob
-from os import path, listdir
+import glob, hou, datetime
+from os import path, listdir, environ
 
 def fixCrowdCache(input_dir, search_path,new_path):
     list_geos = glob.glob(path.join(input_dir, "*.geo"))
@@ -12,3 +12,14 @@ def fixCrowdCache(input_dir, search_path,new_path):
         file.write(tmp)
         file.close()
         print "{} fixed".format(item)
+
+default_render_path = path.join(environ["HIP"], "render/*")
+def checkRenderTime(path_to_folder=default_render_path):
+    files = glob.glob(path_to_folder)
+    mtimes = []
+    for file in files:
+        mt = path.getmtime(file)
+        mtimes.append(mt)
+    mtimes.sort()
+    delta = mtimes[-1] - mtimes[0]
+    return str(datetime.timedelta(seconds=delta))
